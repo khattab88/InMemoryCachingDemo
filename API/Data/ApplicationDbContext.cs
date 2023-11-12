908ebeda0163f539b978bea11d0a629e87088287
khattab88 <m_khatab_88@hotmail.com>
2023-11-12 22:54:43 +0200

    scaffold database

---
+++ API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Driver> Drivers { get; set; }
+    }
+}
---
+++ API/Models/Driver.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public int RacingNumber { get; set; }
+        public string Team { get; set; } = "";
+    }
+}
